ea1daa4f7fa28dde5c6c23cc7a5b6247f9f0cf18
Eric LEGBA <eric.legba@gmail.com>
2024-01-23 12:15:20 +0100

    first commit

--- app/src/app/_services/user.service.spec.ts
+++ app/src/app/_services/user.service.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
+
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule]
+    });
     service = TestBed.inject(UserService);
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
